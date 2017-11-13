92508f154f721d535eb64a78f699482557f8fe7e
Oyanel <th.brune@laposte.net>
2017-11-13 15:17:12 +0100

    pour flo

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    private final static String FILE_4 = "fichierTexte/mots04lettres.txt";
+    private final static String FILE_5 = "fichierTexte/mots05lettres.txt";
+    private final static String FILE_6 = "fichierTexte/mots06lettres.txt";
+    private final static String FILE_7 = "fichierTexte/mots07lettres.txt";
+    private final static String FILE_8 = "fichierTexte/mots08lettres.txt";
+    private final static String FILE_9 = "fichierTexte/mots09lettres.txt";
+    private final static String FILE_10 = "fichierTexte/mots10lettres.txt";
+    private final static String FILE_11 = "fichierTexte/mots11lettres.txt";
+    private final static String FILE_12 = "fichierTexte/mots12lettres.txt";
+    private final static String FILE_13 = "fichierTexte/mots13lettres.txt";
+    private final static String FILE_14 = "fichierTexte/mots14lettres.txt";
+    private final static String FILE_15 = "fichierTexte/mots15lettres.txt";
+    private final static String FILE_16 = "fichierTexte/mots16lettres.txt";
+    private final static String FILE_17 = "fichierTexte/mots17lettres.txt";
+    private final static String FILE_18 = "fichierTexte/mots18lettres.txt";
+
     public static void main(String[] args)  {
-        File file = new File("fichierTexte/mots04lettres.txt");
+        File file = new File(FILE_4);
         List<Mot> mots = null;
         try {
             mots = Import.importer(file);
